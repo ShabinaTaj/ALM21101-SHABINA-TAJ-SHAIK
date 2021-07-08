235fd9dfe3bf7a29ed9508081fec491ac8c45f93
ShabinaTaj <87114818+ShabinaTaj@users.noreply.github.com>
2021-07-08 15:52:50 +0530

    Add files via upload

---
+++ day1/sum.java
@@ -0,0 +1,26 @@
+package day1;
+
+public class sum {
+
+	public static void main(String[] args) {
+		
+		char cc='a';
+		String str="hello";
+		double d=2.45;//64 bit
+		float f=2.45f;//32 bit
+		
+		int i=86;
+		int j=700;
+	
+	//write business logic
+	
+		int sum=i+j;
+	
+	//write output logic
+		
+		System.out.println("The sum of i and j is..:"+sum);
+}
+
+	}
+
+
